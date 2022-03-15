94e1c311fea5865bcee503adc5481b7303759950
ld03945670857 <416888771@qq.com>
2022-03-15 20:15:40 +0800

    完成个人中心功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,8 @@
 $.ajaxPrefilter(function (options) {
     
 
-    options.url = 'http://http://api-breakingnews-web.itheima.net' + options.url
+    // options.url = 'http:http://api-breakingnews-web.itheima.net' + options.url
+    options.url = 'http://ajax.frontend.itheima.net' + options.url
     console.log(options.url)
 
     if (options.url.indexOf('/my/') !== -1){
